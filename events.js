676928d47efc98b76b5aa41e2142e87288348e7f
Alex Rueda U <alexruedau@gmail.com>
2019-01-16 21:03:39 +0100

    Finishing the project using the arrows of the keyboard. Function drawingLine added and switches completed.

--- events.js
+++ events.js
@@ -8,24 +8,50 @@ var keys = {
 console.log(keys);
 
 document.addEventListener("keyup", drawingWithKeyBoard);
+var square = document.getElementById("drawArea");
+var paper = square.getContext("2d");
+var x = 150;
+var y = 150;
+
+drawingLine("red", x-1, y-1, x+1, y+1, paper)
+
+function drawingLine(color, xinitial, yinitial, xfinal, yfinal, myCanvas) {
+  myCanvas.beginPath();
+  myCanvas.strokeStyle = color;
+  myCanvas.lineWidth = 2;
+  myCanvas.moveTo(xinitial, yinitial);
+  myCanvas.lineTo(xfinal, yfinal);
+  myCanvas.stroke();
+  myCanvas.closePath();
+}
 
 function drawingWithKeyBoard(evento) {
 
+  var newColor = "blue";
+  var movement = 10;
   switch(evento.keyCode) {
     case keys.UP:
-      console.log("Up!")
+      drawingLine(newColor, x, y, x, y - movement, paper);
+      y = y - movement;
+      console.log("Up!");
     break;
     case keys.DOWN:
-      console.log("Down!")
+      drawingLine(newColor, x, y, x, y + movement, paper);
+      y = y + movement;
+      console.log("Down!");
     break;
     case keys.RIGHT:
-      console.log("Right!")
+      drawingLine(newColor, x, y, x + movement, y, paper);
+      x = x + movement;
+      console.log("Right!");
     break;
     case keys.LEFT:
-      console.log("Left!")
+      drawingLine(newColor, x, y, x - movement, y, paper);
+      x = x - movement;
+      console.log("Left!");
     break;
     default:
-      console.log("Other key")
+      console.log("Other key");
     break;
   }
 
