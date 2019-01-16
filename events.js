3ff9c5a0e0981998f5124d274d86fd3d93826df8
Alex Rueda U <alexruedau@gmail.com>
2019-01-16 19:27:29 +0100

    Using keyCode and switch conditions

--- events.js
+++ events.js
@@ -1,5 +1,32 @@
-document.addEventListener("keydown", drawingWithKeyBoard);
+var keys = {
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+  LEFT: 37
+};
 
-function drawingWithKeyBoard() {
-  alert("OMG!");
+console.log(keys);
+
+document.addEventListener("keyup", drawingWithKeyBoard);
+
+function drawingWithKeyBoard(evento) {
+
+  switch(evento.keyCode) {
+    case keys.UP:
+      console.log("Up!")
+    break;
+    case keys.DOWN:
+      console.log("Down!")
+    break;
+    case keys.RIGHT:
+      console.log("Right!")
+    break;
+    case keys.LEFT:
+      console.log("Left!")
+    break;
+    default:
+      console.log("Other key")
+    break;
+  }
+
 }
